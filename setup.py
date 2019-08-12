378294b35933a5d1ef4e0047b9ab8d6e361ec765
andersh <anders.haal@ingby.com>
2019-08-12 17:34:54 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='monitor-promdiscovery',
+    version='0.0.1',
+    packages=find_packages(),
+    author='thenodon',
+    author_email='anders@opsdis.com',
+    url='https://bitbucket.org/opsdis/monitor-promdiscovery',
+    license='GPLv3',
+    include_package_data=True,
+    zip_safe=False,
+
+)
