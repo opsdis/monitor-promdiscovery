1bfa2065f5b90f4da8b9c8b923fbd28144b9b8db
andersh <anders.haal@ingby.com>
2021-06-27 12:38:14 +0200

    Use git tag for version

--- setup.py
+++ setup.py
@@ -1,8 +1,24 @@
-from setuptools import find_packages, setup
+from os.path import dirname, join
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    return open(join(dirname(__file__), fname)).read()
+
 
 setup(
     name='monitor-promdiscovery',
-    version='0.0.3',
+    version_config={
+        "template": "{tag}",
+        "dev_template": "{tag}.dev{ccount}",
+        "dirty_template": "{tag}.post{ccount}+git.{sha}.dirty",
+        "starting_version": "0.0.1",
+        "version_callback": None,
+        "version_file": None,
+        "count_commits_from_version_file": False,
+        "branch_formatter": None
+    },
+    setup_requires=['setuptools-git-versioning'],
     packages=find_packages(),
     author='thenodon',
     author_email='anders@opsdis.com',
@@ -10,5 +26,7 @@ setup(
     license='GPLv3',
     include_package_data=True,
     zip_safe=False,
-
+    description='A Prometheus file based service discovery for Icinga2 and OP5 Monitor',
+    install_requires=read('requirements.txt').split(),
+    python_requires='>=3.6',
 )
